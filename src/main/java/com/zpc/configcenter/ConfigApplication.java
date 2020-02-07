a94dadb8662e6eabc6798ab134ea71d7b03623e4
siory <siory123@qq.com>
2020-02-07 23:31:02 +0800

    init

---
+++ src/main/java/com/zpc/configcenter/ConfigApplication.java
@@ -0,0 +1,14 @@
+package com.zpc.configcenter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+/**
+ * 开启配置服务
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
