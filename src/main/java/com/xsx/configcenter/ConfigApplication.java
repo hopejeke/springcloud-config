8689939474333e3851b89ccfb8a8a1ed21c6c71e
siory <siory123@qq.com>
2020-02-09 20:42:02 +0800

    init

--- src/main/java/com/xsx/configcenter/ConfigApplication.java
+++ src/main/java/com/xsx/configcenter/ConfigApplication.java
@@ -2,11 +2,15 @@ package com.xsx.configcenter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * 开启配置服务
  */
 @EnableConfigServer
 @SpringBootApplication
+@EnableEurekaClient
+
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
